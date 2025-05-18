6bb981cb7af9e585f89349db6a71de421746af40
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-17 19:38:29 -0600

    add authMiddleware file and set up express app in index.js filee

---
+++ server/src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { authMiddleware } from "./middleware/authMiddleware";
+/* ROUTE IMPORT */
+import customerRoutes from "./routes/customerRoutes";
+import restaurantRoutes from "./routes/restaurantRoutes";
+import driverRoutes from "./routes/driverRoutes";
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("This is home route");
+});
+
+app.use("/customers", authMiddleware(["customer"]), customerRoutes);
+app.use("/restaurants", authMiddleware(["restaurant"]), restaurantRoutes);
+app.use("/drivers", authMiddleware(["driver"]), driverRoutes);
+
+/* SERVER */
+const port = Number(process.env.PORT) || 3002;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
+});
