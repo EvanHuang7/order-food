2e9d304885ba29c2297e1b9919ca6ac4b570d66a
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-18 00:31:04 -0600

    add createNewUserInDatabase function and withToast function in utils.ts file

---
+++ client/src/lib/utils.ts
@@ -0,0 +1,72 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { toast } from "sonner";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatEnumString(str: string) {
+  return str.replace(/([A-Z])/g, " $1").trim();
+}
+
+type MutationMessages = {
+  success?: string;
+  error: string;
+};
+
+export const withToast = async <T>(
+  mutationFn: Promise<T>,
+  messages: Partial<MutationMessages>
+) => {
+  const { success, error } = messages;
+
+  try {
+    const result = await mutationFn;
+    if (success) toast.success(success);
+    return result;
+  } catch (err) {
+    if (error) toast.error(error);
+    throw err;
+  }
+};
+
+export const createNewUserInDatabase = async (
+  user: any,
+  idToken: any,
+  userRole: string,
+  fetchWithBQ: any
+) => {
+  // Map roles to API endpoints
+  let createEndpoint = "";
+  switch (userRole?.toLowerCase()) {
+    case "customer":
+      createEndpoint = "/customers";
+      break;
+    case "restaurant":
+      createEndpoint = "/restaurants";
+      break;
+    case "driver":
+      createEndpoint = "/drivers";
+      break;
+    default:
+      throw new Error(`Invalid user role: ${userRole}`);
+  }
+
+  const createUserResponse = await fetchWithBQ({
+    url: createEndpoint,
+    method: "POST",
+    body: {
+      cognitoId: user.userId,
+      name: user.username,
+      email: idToken?.payload?.email || "",
+      phoneNumber: "",
+    },
+  });
+
+  if (createUserResponse.error) {
+    throw new Error("Failed to create user record");
+  }
+
+  return createUserResponse;
+};
