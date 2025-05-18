7deed8b0521e59b4be4fbedb14dff689ceec9c96
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-18 00:46:53 -0600

    initailize root layout and page and global.css files

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="h-full w-full">
+      <Navbar />
+      <main className={`h-full flex w-full flex-col`}>home</main>
+    </div>
+  );
+}
