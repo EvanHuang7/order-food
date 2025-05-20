ac00defce5035f9690a43f557126b1215047a190
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-20 15:43:59 -0600

    add Auto-slide to imagePreview

--- client/src/app/(home)/[id]/ImagePreviews.tsx
+++ client/src/app/(home)/[id]/ImagePreviews.tsx
@@ -2,10 +2,11 @@
 
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ImagePreviews = ({ images }: ImagePreviewsProps) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handlePrev = () => {
     setCurrentImageIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
@@ -15,8 +16,25 @@ const ImagePreviews = ({ images }: ImagePreviewsProps) => {
     setCurrentImageIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
   };
 
+  // Auto-slide unless hovered
+  useEffect(() => {
+    if (isHovered) return;
+
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prev) =>
+        prev === images.length - 1 ? 0 : prev + 1
+      );
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images.length, isHovered]);
+
   return (
-    <div className="relative h-[450px] w-full">
+    <div
+      className="relative h-[450px] w-full"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       {images.map((image, index) => (
         <div
           key={image}
@@ -28,7 +46,7 @@ const ImagePreviews = ({ images }: ImagePreviewsProps) => {
             src={image}
             alt={`Property Image ${index + 1}`}
             fill
-            priority={index == 0}
+            priority={index === 0}
             className="object-cover cursor-pointer transition-transform duration-500 ease-in-out"
           />
         </div>
@@ -43,7 +61,7 @@ const ImagePreviews = ({ images }: ImagePreviewsProps) => {
       <button
         onClick={handleNext}
         className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-primary-700 bg-opacity-50 p-2 rounded-full focus:outline-none focus:ring focus:ring-secondary-300"
-        aria-label="Previous image"
+        aria-label="Next image"
       >
         <ChevronRight className="text-white" />
       </button>
