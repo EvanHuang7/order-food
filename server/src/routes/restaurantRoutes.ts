ac920388fae9a732f6aa211aa922ebb06e78c9c3
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-19 19:11:31 -0600

    update restaurant routes auth acess

--- server/src/routes/restaurantRoutes.ts
+++ server/src/routes/restaurantRoutes.ts
@@ -9,8 +9,16 @@ import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-router.get("/:cognitoId", getRestaurant);
-router.get("/", getRestaurants);
+router.get(
+  "/:cognitoId",
+  authMiddleware(["customer", "restaurant", "driver"]),
+  getRestaurant
+);
+router.get(
+  "/",
+  authMiddleware(["customer", "restaurant", "driver"]),
+  getRestaurants
+);
 router.post("/", authMiddleware(["restaurant"]), createRestaurant);
 router.put("/:cognitoId", authMiddleware(["restaurant"]), updateRestaurant);
 
