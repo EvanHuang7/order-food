98f4b714ecc8ee58290c6bafd3d9f6844ed3f2b1
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-23 22:06:47 -0600

    add restaurant earnings page

--- client/src/app/(dashboard)/driver/earnings/page.tsx
+++ client/src/app/(dashboard)/driver/earnings/page.tsx
@@ -2,16 +2,14 @@
 
 import EarningsHistory from "@/components/EarningsHistory";
 import Loading from "@/components/Loading";
-import { useGetAuthUserQuery, useGetOrdersQuery } from "@/state/api";
+import { useGetOrdersQuery } from "@/state/api";
 import React from "react";
 
 const Earnings = () => {
-  const { data: authUser } = useGetAuthUserQuery();
-
   const { data: orders, isLoading, isError } = useGetOrdersQuery();
 
   if (isLoading) return <Loading />;
-  if (!orders || isError) return <div>Error loading payments</div>;
+  if (!orders || isError) return <div>Error loading earnings</div>;
 
   return (
     <div className="dashboard-container">
--- client/src/app/(dashboard)/restaurant/earnings/page.tsx
+++ client/src/app/(dashboard)/restaurant/earnings/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import EarningsHistory from "@/components/EarningsHistory";
+import Loading from "@/components/Loading";
+import { useGetOrdersQuery } from "@/state/api";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Earnings = () => {
+  const { data: orders, isLoading, isError } = useGetOrdersQuery();
+
+  if (isLoading) return <Loading />;
+  if (!orders || isError) return <div>Error loading earnings</div>;
+
+  return (
+    <div className="dashboard-container">
+      <EarningsHistory orders={orders || []} userType={"restaurant"} />
+    </div>
+  );
 };
 
-export default page;
+export default Earnings;
