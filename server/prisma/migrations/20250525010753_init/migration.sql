ffa229b96269cd3b78143fa50c11f3300aec4d56
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-24 19:12:03 -0600

    add creating trigger script to sql file and rerun "npx prisma migrate reset" cli

--- server/prisma/migrations/20250525010753_init/migration.sql
+++ server/prisma/migrations/20250525010753_init/migration.sql
@@ -294,3 +294,20 @@ ALTER TABLE "NotificationSetting" ADD CONSTRAINT "NotificationSetting_customerId
 
 -- AddForeignKey
 ALTER TABLE "Notification" ADD CONSTRAINT "Notification_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- 1. Create the trigger function
+CREATE OR REPLACE FUNCTION notify_new_notification()
+RETURNS TRIGGER AS $$
+BEGIN
+  PERFORM pg_notify('new_notification_channel', row_to_json(NEW)::text);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- 2. Create the trigger
+CREATE TRIGGER on_new_notification
+AFTER INSERT ON "Notification"
+FOR EACH ROW
+EXECUTE FUNCTION notify_new_notification();
+
+
